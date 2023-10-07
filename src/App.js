3f4583f625573ed6ba14bab1bff1f8c7ab6d0b8e
kanithkar <kanithkar@xerago.com>
2023-10-07 13:59:21 +0530

    subscribe funcionality done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import Main from "./GridX/Components/Main";
 import HookEffect from "./components/UseEffect"
 import Popup from "./GridX/Components/Popup";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import './GridX/Css/gridx.css'
 import { AiOutlineCloseCircle } from "react-icons/ai";
 import Header from './GridX/Components/Header'
@@ -17,24 +17,63 @@ import { SlMagicWand } from "react-icons/sl";
 function App() {
   const [subscribe, subscribed] = useState(false);
   const [popClose, popOpen] = useState(false);
+  const [formDate,setFormData] = useState({
+    name:'',
+    email:'',
+    feedback:''
+  })
 
   setTimeout(()=>{
     const allTag = document.querySelectorAll("a");
-    console.log(allTag)
+   
     allTag.forEach(element => {
       element.addEventListener("click", function (e) {
-        e.preventDefault();
         subscribeHandle(e);
       })
     });
   },300)
  
   function subscribeHandle(e){
-    e.preventDefault();
-   
-    popOpen(!popClose);
-  
+    
+    var userNameStore = sessionStorage.getItem("name");
+    var userEmailStore = sessionStorage.getItem("email")
+    if (userEmailStore || userNameStore){
+      subscribed(true)
+    }else{
+      e.preventDefault();
+      popOpen(!popClose);
+    }
   }
+  
+
+  //form input onchange funtion
+  const inputChange = (e) =>{
+    const { name, value } = e.target;
+   setFormData({
+    ...formDate,
+    [name]:value
+   })
+  }
+
+  //form submit function 
+  const submitHandle = (e) =>{
+    e.preventDefault();
+    const {name, email} =formDate;
+    sessionStorage.setItem('name', name);
+    sessionStorage.setItem('email', email);
+    popOpen(!popClose);
+  }
+
+
+  //loading time user login check 
+  useEffect(()=>{
+    var userNameStore = sessionStorage.getItem("name");
+    var userEmailStore = sessionStorage.getItem("email");
+    if (userEmailStore || userNameStore) {
+      subscribed(true)
+    }
+  }, [Popup])
+
 
   return (
     <div className="bg-[var(--bg-color)] pb-10">
@@ -49,33 +88,33 @@ function App() {
           <div className="  w-[100%] mx-auto h-full relative pt-5">
             <div className="text-white  shadow-box-overlay text-xl font-semibold">
              
-              <form className="p-5">
+              <form onSubmit={submitHandle} className="p-5 absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] z-40 w-full">
                 <h2 className="text-center text-4xl pb-2"> Welcome to My Portfolio!</h2>
 
                 <p className="pb-5 text-center text-sm text-[var(--color)]"><BiHappyAlt className="text-xl inline-block text-[#4dffa5] mr-2"></BiHappyAlt>Discover, Enjoy, and Connect with My Work!<SlMagicWand className="text-xl inline-block text-[#d22af1]"></SlMagicWand></p>
                 <div className='inner-shadow   relative'>
                   <div className='shadow-box-overlay '>
-                    <input placeholder="Enter Your Name*" type="text" className="focus:border-none focus:outline-none bg-transparent relative z-50 w-full text-[15px] py-2 px-5" />
+                    <input placeholder="Enter Your Name*" name="name" onChange={inputChange} value={formDate.name} type="text" className="focus:border-none focus:outline-none bg-transparent relative z-50 w-full text-[15px] py-2 px-5" />
                   </div>
                 </div>
                 <div className='inner-shadow   relative mt-8'>
                   <div className='shadow-box-overlay '>
-                    <input placeholder="Enter Your Mail*" type="text" className="focus:border-none focus:outline-none bg-transparent relative z-50 w-full text-[15px] py-2 px-5" />
+                    <input placeholder="Enter Your Mail*" name="email" onChange={inputChange} value={formDate.email} type="text" className="focus:border-none focus:outline-none bg-transparent relative z-50 w-full text-[15px] py-2 px-5" />
                   </div>
                 </div>
                 {/* <div className='inner-shadow   relative mt-8'>
                   <div className='shadow-box-overlay '>
-                    <input placeholder="FeedBack*" type="text" className="focus:border-none focus:outline-none bg-transparent relative z-50 w-full text-[15px] py-2 px-5" />
+                    <input placeholder="FeedBack*" type="text" name="feedback"  onChange={inputChange} value={formDate.name} className="focus:border-none focus:outline-none bg-transparent relative z-50 w-full text-[15px] py-2 px-5" />
                   </div>
                 </div> */}
-                
+
                 <div className='inner-shadow flex justify-center mx-auto relative mt-8'>
                   <div className=' inline-block grow   ease-out duration-300 '>
                     <input type="submit" className="hover:bg-[#fff] hover:text-[var(--color)] shadow-box-overlay bg-transparent relative z-50 w-full text-lg py-2 px-10 text-white font-2xl  rounded-[20px]   cursor-pointer inline-block" />
                   </div>
                 </div>
-                <p className="pt-5 md:w-[80%] text-2xl mx-auto text-center">Not interested? No worries! There's a "Not Interested" option too.<BiHappyAlt className="text-2xl inline-block text-[#ffef17] mr-2"></BiHappyAlt></p>
-
+                <p className="pt-10 md:w-[80%]   text-2xl mx-auto text-center">Not interested? No worries! There's a "Not Interested" option too.<BiHappyAlt className="text-2xl inline-block text-[#ffef17] mr-2"></BiHappyAlt></p>
+                <button className="shadow-box-overlay bg-transparent relative z-50 w-full text-lg  px-10 text-white font-2xl  rounded-[20px] border-none py-2 my-5  cursor-pointer inline-block">Not Interested</button>
               </form>
             </div>
           </div>
--- src/GridX/Components/Popup.js
+++ src/GridX/Components/Popup.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PortfollioAbout from './PortfollioAbout';
 import '../Css/gridx.css';
 import { useState, useEffect } from 'react';
+import { BiHappyAlt } from "react-icons/bi";
 
 function Popup() {
 
@@ -13,7 +14,7 @@ function Popup() {
             "https://dummyjson.com/quotes/random"
         ).then((response) => response.json());
         getQuate(response);
-        
+
     };
 
     useEffect(() => {
@@ -27,48 +28,67 @@ function Popup() {
         "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
     ];
 
-    const month = curDeptMonth[(currentDate.getMonth()-1)]
-  return (
-      <div className='bg-[var(--bg-color)] pb-10'>
+    const month = curDeptMonth[(currentDate.getMonth() - 1)];
 
-          <div className='container  m-auto w-[90%] mx-auto sm:w-full mt-10'>
-              <div className='items-center flex   flex-col  sm:flex-row  p-10'>
-                  <div className='w-[35%] shadow-box-overlay relative p-5 '>
-                      <div className='flex-none  bg-gradient-to-r from-blue-400 via-blue-200 to-blue-500 w-[340px] h-[330px]  rounded-[30px] mx-auto overflow-hidden'>
-                          <a href='#' className='inline-block'>
-                              <img src='./Image/me2.png' className='object-fit' alt='logo' />
-                          </a>
-                      </div>
-                  </div>
-                <div className='relative  w-[65%] ml-5'>
-                      <div className='shadow-box-overlay pt-8 px-6 w-[80%]'>
-                          <h2 className='text-4xl text-white font-semibold'>Welcome to My Portfolio</h2>
-                          <p className='pt-2'>My name is Kanith. I'm a web developer passionate about creating user-friendly websites. Skilled in HTML, CSS, and JavaScript. Explore my exciting projects!</p>
+
+    //username check in seassion storage 
+    const useName = sessionStorage.getItem("name");
+    return (
+        <div className='bg-[var(--bg-color)] pb-10'>
+
+            <div className='container  m-auto w-[90%] mx-auto sm:w-full mt-10'>
+                <div className='items-center flex   flex-col  sm:flex-row  p-10'>
+                    <div className='w-[35%] self-center shadow-box-overlay relative p-5 '>
+                        <div className='flex-none  bg-gradient-to-r from-blue-400 via-blue-200 to-blue-500 w-[340px] h-[330px]  rounded-[30px] mx-auto overflow-hidden'>
+                            <a href='#' className='inline-block'>
+                                <img src='./Image/me2.png' className='object-fit' alt='logo' />
+                            </a>
+                        </div>
                     </div>
-                      <div className='flex items-end justify-between px-5 pb-8'>
-                          <div className='w-[80%]'>
-                              <p className='text-2xl text-white font-semibold  pb-1 pt-10'>Here's a fun quote to brighten your day:</p>
-                            
+                    <div className='relative  w-[65%] ml-5'>
+                        <div className={
+                            useName ? 'flex justify-between items-center py-5 px-0': 'hidden'
+                        }>
+                            <img
+                                className='flex-none'
+                                alt='star'
+                                src='./Image/star-2.png' />
+                            <div className='flex-1 '>
+                                <h2 className='text-2xl w-7/10 text-white font-semibold  text-center mx-auto w-full md:w-5/6'><span className='text-[#efff00]'>{useName} <BiHappyAlt className="text-2xl inline-block text-[#00ff08] "></BiHappyAlt></span>, it's time to dive into My Project. We're in for some coding fun!</h2>
 
-                                  {
-                                      quate && (<h3 className=' text-lg text-[var(--color)]' > {quate.quote}    </h3>)
-                                     
-                                  }
+                            </div>
+                            <img className='flex-none' src='./Image/star-2.png' />
+                        </div>
+                        <div className='relative'>
+                            <div className='shadow-box-overlay pt-8 px-6 w-[80%]'>
+                                <h2 className='text-4xl text-white font-semibold'>Welcome to My Portfolio</h2>
+                                <p className='pt-2'>My name is Kanith. I'm a web developer passionate about creating user-friendly websites. Skilled in HTML, CSS, and JavaScript. Explore my exciting projects!</p>
+                            </div>
+                            <div className='flex items-end justify-between px-5 pb-8'>
+                                <div className='w-[80%]'>
+                                    <p className='text-2xl text-white font-semibold  pb-1 pt-10'>Here's a fun quote to brighten your day:</p>
 
-                                
-                          </div>
 
-                          <div className='float-right'>
-                              <p className='text-xl text-white font-semibold  pb-1 pt-10'>Date</p>
-                              <p>{`${month} ${currentDate.getDate()}, ${currentDate.getFullYear()}`}</p>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-         </div>
+                                    {
+                                        quate && (<h3 className=' text-lg text-[var(--color)]' > {quate.quote}    </h3>)
 
-    </div>
-  )
+                                    }
+
+
+                                </div>
+
+                                <div className='float-right'>
+                                    <p className='text-xl text-white font-semibold  pb-1 pt-10'>Date</p>
+                                    <p>{`${month} ${currentDate.getDate()}, ${currentDate.getFullYear()}`}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default Popup
